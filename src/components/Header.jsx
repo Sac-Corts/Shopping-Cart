5d84ea58d517c02a03f606b2ce5a4db2b2b0ad22
Sac-Corts <icortesmorales21@gmail.com>
2024-10-01 23:39:56 -0500

    Hook - useCart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable react/prop-types */
-import { useMemo } from "react"
 
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart }) {
+export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }) {
 
-    // State Derivado
-    const isEmpty = useMemo( () => cart.length === 0, [cart])
-    const cartTotal = useMemo( () => cart.reduce( (total, item) => total + (item.quantity * item.price), 0), [cart]) 
 
     return (
         <header className="py-5 header">
